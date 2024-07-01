7809fa2c82fcdd0177654a27fdcc107d36e3b033
Darccelio Silva <darccelio@gmail.com>
2024-06-30 23:05:30 -0300

    cadastrar aluno/educador e logout

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -10,16 +10,14 @@ export const AuthProvider = ({ children }) => {
     const userToken = localStorage.getItem('user_token')
     const usersStorage = localStorage.getItem('users_bd')
 
+    console.log(`userToken; ${userToken}`)
+    console.log(`usersStorage; ${usersStorage}`)
 
-    console.log(`userToken; ${userToken}`);
-    console.log(`usersStorage; ${usersStorage}`);
-
-    if (userToken && usersStorage) {      
-      if(JSON.parse(usersStorage)?.email ===JSON.parse(userToken)?.email)
+    if (userToken && usersStorage) {
+      if (JSON.parse(usersStorage)?.email === JSON.parse(userToken)?.email)
         setUser(JSON.parse(usersStorage))
     }
   }, [])
-
 
   //-----------------------------------------------------------------------------------------
   const signin = async (email, password) => {
@@ -28,66 +26,59 @@ export const AuthProvider = ({ children }) => {
 
     const user = {
       email: email,
-      senha: password
-    }  
-    
-    if(!user.email && !user.senha)
-      return 'Preencha os campos corretamente!'
+      senha: password,
+    }
+
+    if (!user.email && !user.senha) return 'Preencha os campos corretamente!'
 
     try {
-
       const response = await fetch(URL_TO_AUTH, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           'Accept-Type': 'application/json',
         },
-        body: JSON.stringify(user)
+        body: JSON.stringify(user),
       })
 
       if (!response.ok) {
         // Trata a resposta não bem-sucedida (ex: erro 400 ou 500)
-        const errorData = await response.json();
-        setEr(errorData.message || 'Erro ao autenticar usuário');
+        const errorData = await response.json()
+        setEr(errorData.message || 'Erro ao autenticar usuário')
         // return errorData.message || 'Erro ao registrar usuário';
-        return;
+        return
       }
 
       const json = await response.json()
 
       if (json?.success) {
         const accessToken = json.data.accessToken
-        const token = `Bearer ${accessToken}`          
+        const token = `Bearer ${accessToken}`
 
         localStorage.setItem('user_token', JSON.stringify({ email, token }))
         setUser(user)
       }
-
-
-    }catch (error) {
+    } catch (error) {
       console.error('Erro na requisição de signin:', error)
       // return error.message
       setEr(error.message || 'Erro ao autenticar usuário')
     }
-    
   }
 
   //-----------------------------------------------------------------------------------------
   const signup = async (email, password, passwordConfirmed, papel) => {
     if (password !== passwordConfirmed) {
-      return 'As senhas não coincidem';
+      return 'As senhas não coincidem'
     }
-  
+
     const user = {
       email: email,
       papel: papel,
       senha: password,
-      confirmacaoSenha: passwordConfirmed
-    }  
+      confirmacaoSenha: passwordConfirmed,
+    }
 
-    let isAutenticado = false;
-
-    try {  
+    try {
       const response = await fetch(
         'https://localhost:5000/api/conta/registrar',
         {
@@ -99,37 +90,104 @@ export const AuthProvider = ({ children }) => {
 
       if (!response.ok) {
         // Trata a resposta não bem-sucedida (ex: erro 400 ou 500)
-        const errorData = await response.json();
-        setUser({...user, isAutenticado: false})
-        return errorData.message || 'Erro ao registrar usuário';
+        const errorData = await response.json()
+        setUser({ ...user, isAutenticado: false })
+        return errorData.message || 'Erro ao registrar usuário'
       }
 
       const json = await response.json()
       const accessToken = json.data.accessToken
       const token = `Bearer ${accessToken}`
-  
-      localStorage.setItem("users_bd", JSON.stringify({ email, password, papel }));
-      localStorage.setItem("user_token", JSON.stringify({ email, token }));
 
-      setUser({ email, password, papel, isAutenticado: true});
+      localStorage.setItem(
+        'users_bd',
+        JSON.stringify({ email, password, papel })
+      )
+      localStorage.setItem('user_token', JSON.stringify({ email, token }))
 
+      setUser({ email, password, papel, isAutenticado: true })
     } catch (error) {
-      console.error('Erro na requisição de signup:', error)      
-      setUser({...user, isAutenticado: false})
+      console.error('Erro na requisição de signup:', error)
+      setUser({ ...user, isAutenticado: false })
       return error.message
     }
   }
 
   //-----------------------------------------------------------------------------------------
 
-  const signout = () => {
+  const registerPessoa = async (pessoa) => {
+  
+    if(!pessoa) {
+      setUser({ ...user, isAutenticado: false })
+      return 'Dados de Pessoa inválido'
+    }
+
+    const recurso = pessoa.registroConselho != null ? 'educadorfisico' : 'alunos';
+    const URL = `https://localhost:5000/api/${recurso}`
+    
+    try {
+      const userToken = localStorage.getItem('user_token');
+
+
+      debugger
+      if(userToken === null) {
+        setEr('Usuario não está logado');
+        setUser({ ...user, isAutenticado: false })
+        return
+      }
+
+      const jwt = JSON.parse(userToken).token;
+
+      let headersAuthorization = {
+        'Authorization': `${jwt}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      };
+
+      const response = await fetch(
+        URL,
+        {
+          method: 'POST',
+          headers: headersAuthorization,
+          body: JSON.stringify( pessoa),
+        }
+      )
+
+      if (!response.ok) {
+        // Trata a resposta não bem-sucedida (ex: erro 400 ou 500)
+        const errorData = await response.json()
+        setUser({ ...user, isAutenticado: false })
+        return errorData.message || 'Erro ao registrar usuário'
+      }
+
+      const json = await response.json()
+
+      setUser({ ...user, isAutenticado: true })
+
+    } catch (error) {
+      console.error('Erro na requisição de signup:', error)
+      setUser({ ...user, isAutenticado: false })
+      return error.message
+    }
+  }
+
+  //-----------------------------------------------------------------------------------------
+
+  const signout = async () => {
     setUser(null)
     localStorage.removeItem('user_token')
+
+    const response = await fetch(
+      'https://localhost:5000/api/conta/sair',
+      {
+        method: 'POST',
+      }
+    )
   }
 
   return (
     <AuthContext.Provider
-      value={{ user, signed: !!user, signin, signup, signout }}
+      value={{ user, signed: !!user, signin, signup, signout, registerPessoa }}
     >
       {children}
     </AuthContext.Provider>
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -7,28 +7,31 @@ import useAuth from '../../hooks/useAuth'
 
 const Signup = () => {
   const [email, setEmail] = useState('')
-  // const [emailConf, setEmailConf] = useState("");
   const [senha, setSenha] = useState('')
   const [senhaConfirmada, setSenhaConfirmada] = useState('')
   const [papel, setPapel] = useState('')
   const [error, setError] = useState('')
+
+  const [nome, setNome] = useState('')
+  const [cpf, setCpf] = useState('')
+  const [registroProf, setRegistroProf] = useState('')
+  const [isUserRegistered, setIsUserRegistered] = useState(false)
+
+  const { signup, registerPessoa } = useAuth()
   const navigate = useNavigate()
 
-  const { signup } = useAuth()
+  const Private = () => {
+    const { signed } = useAuth();  
+    return signed > 0;
+  };
 
   const handleSignup = async () => {
-    // Tornar a função assíncrona
+    
     if (!email || !senha || !senhaConfirmada || !papel) {
-      // Verificar se 'papel' também está preenchido
       setError('Preencha todos os campos')
       return
     }
-    // } else if (email !== emailConf) {
-    //   setError("Os e-mails não são iguais");
-    //   return;
-    // }
 
-    debugger
     const res = await signup(email, senha, senhaConfirmada, papel)
 
     if (res) {
@@ -37,6 +40,41 @@ const Signup = () => {
     }
 
     alert('Usuário cadatrado com sucesso!')
+    // navigate('/')
+
+    setIsUserRegistered(true)
+
+  }
+
+  const isLog = Private();
+
+  const handleAdditionalInfoSubmit = async () => {
+    // Lógica para salvar nome e CPF
+
+    debugger
+    if (!nome || !cpf && (papel === "0" && !registroProf)) {
+      setError('Preencha todos os campos')
+      return
+    }
+
+    const pessoa = {
+      pessoaCreateRequest: {
+       cpf, nome, emailUsuario: email
+      }
+    }
+
+    if(papel === "0") 
+      pessoa.registroConselho = registroProf
+
+    const res = await registerPessoa(pessoa)
+
+
+    if (res) {
+      setError(res)
+      return
+    }
+
+    alert('Pessoa cadatrada com sucesso!')
     navigate('/')
   }
 
@@ -44,73 +82,87 @@ const Signup = () => {
     <C.Container>
       <C.Label>Improveu</C.Label>
       <C.Content>
-        <Input
-          type="email"
-          placeholder="Digite seu E-mail"
-          value={email}
-          onChange={e => [setEmail(e.target.value), setError('')]}
-        />
-        {/* <Input
-          type="email"
-          placeholder="Confirme seu E-mail"
-          value={emailConf}
-          onChange={(e) => [setEmailConf(e.target.value), setError("")]}
-        /> */}
-        <Input
-          type="password"
-          placeholder="Digite sua Senha"
-          value={senha}
-          onChange={e => [setSenha(e.target.value), setError('')]}
-        />
-        <Input
-          type="password"
-          placeholder="Confirme sua Senha"
-          value={senhaConfirmada}
-          onChange={e => [setSenhaConfirmada(e.target.value), setError('')]}
-        />
-        {/* <Input // Campo de entrada para 'papel'
-          type="text"
-          placeholder="Digite seu Papel"
-          value={papel}
-          onChange={(e) => { setPapel(e.target.value); setError(""); }}
-        /> */}
-        <div>
-          <label>
-            <input
-              type="radio"
-              value={0}
-              id="edFisico"
-              name="tipo-papel"
-              onChange={e => {
-                setPapel(e.target.value)
-                setError('')
-              }}
+        {!isUserRegistered ? (
+          <>
+            <Input
+              type="email"
+              placeholder="Digite seu E-mail"
+              value={email}
+              onChange={e => [setEmail(e.target.value), setError('')]}
             />
-            Educador Físico
-          </label>
-          <br />
-          <label>
-            <input
-              type="radio"
-              value={1}
-              id="aluno"
-              name="tipo-papel"
-              onChange={e => {
-                setPapel(e.target.value)
-                setError('')
-              }}
+            <Input
+              type="password"
+              placeholder="Digite sua Senha"
+              value={senha}
+              onChange={e => [setSenha(e.target.value), setError('')]}
             />
-            Aluno
-          </label>
-        </div>
-        <C.labelError>{error}</C.labelError>
-        <Button Text="Inscrever-se" onClick={handleSignup} />
-        <C.LabelSignin>
-          Já tem uma conta?
-          <C.Strong>
-            <Link to="/">&nbsp;Entre</Link>
-          </C.Strong>
-        </C.LabelSignin>
+            <Input
+              type="password"
+              placeholder="Confirme sua Senha"
+              value={senhaConfirmada}
+              onChange={e => [setSenhaConfirmada(e.target.value), setError('')]}
+            />
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value={0}
+                  id="edFisico"
+                  name="tipo-papel"
+                  onChange={e => {
+                    setPapel(e.target.value)
+                    setError('')
+                  }}
+                />
+                Educador Físico
+              </label>
+              <br />
+              <label>
+                <input
+                  type="radio"
+                  value={1}
+                  id="aluno"
+                  name="tipo-papel"
+                  onChange={e => {
+                    setPapel(e.target.value)
+                    setError('')
+                  }}
+                />
+                Aluno
+              </label>
+            </div>
+            <C.labelError>{error}</C.labelError>
+            <Button Text="Inscrever-se" onClick={handleSignup} />
+            <C.LabelSignin>
+              Já tem uma conta?
+              <C.Strong>
+                <Link to="/">&nbsp;Entre</Link>
+              </C.Strong>
+            </C.LabelSignin>
+          </>
+        ) : (
+          <>
+            <Input
+              type="text"
+              placeholder="Digite seu Nome"
+              value={nome}
+              onChange={e => setNome(e.target.value)}
+            />
+            <Input
+              type="text"
+              placeholder="Digite seu CPF"
+              value={cpf}
+              onChange={e => setCpf(e.target.value)}
+            />
+            {papel === "0" && <Input
+              type="text"
+              placeholder="Digite seu numero do Registro Profissional:"
+              value={registroProf}
+              onChange={e => setRegistroProf(e.target.value)}
+            />}
+            <Button Text="Salvar" onClick={handleAdditionalInfoSubmit} />
+          </>
+        )}
       </C.Content>
     </C.Container>
   )
